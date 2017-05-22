19a96ea45f2952b0206e814fb95b40d5b97e801a
Jeremy Landis <jeremylandis@hotmail.com>
2017-05-21 20:40:55 -0400

    [ci] Correct checkstyle issue using tabs instead of spaces

--- src/main/java/org/mybatis/caches/couchbase/CouchbaseCache.java
+++ src/main/java/org/mybatis/caches/couchbase/CouchbaseCache.java
@@ -26,47 +26,44 @@ import org.apache.ibatis.cache.Cache;
  */
 public final class CouchbaseCache implements Cache {
 
-	@Override
-	public String getId() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public String getId() {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-	@Override
-	public void putObject(Object key, Object value) {
-		// TODO Auto-generated method stub
-		
-	}
+  @Override
+  public void putObject(Object key, Object value) {
+    // TODO Auto-generated method stub
+  }
 
-	@Override
-	public Object getObject(Object key) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public Object getObject(Object key) {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-	@Override
-	public Object removeObject(Object key) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public Object removeObject(Object key) {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-	@Override
-	public void clear() {
-		// TODO Auto-generated method stub
-		
-	}
+  @Override
+  public void clear() {
+    // TODO Auto-generated method stub
+  }
 
-	@Override
-	public int getSize() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+  @Override
+  public int getSize() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
 
-	@Override
-	public ReadWriteLock getReadWriteLock() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+  @Override
+  public ReadWriteLock getReadWriteLock() {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
 }
