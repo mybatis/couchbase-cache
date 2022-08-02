88ef6b2749c67f4bd42f48d1fdd0adf8021d3ce2
Jeremy Landis <jeremylandis@hotmail.com>
2022-08-01 21:25:10 -0400

    [ci] Cleanup javadocs

--- src/main/java/org/mybatis/caches/couchbase/package-info.java
+++ src/main/java/org/mybatis/caches/couchbase/package-info.java
@@ -13,4 +13,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
+/**
+ * Mybatis Cache for Couchbase Package.
+ */
 package org.mybatis.caches.couchbase;
